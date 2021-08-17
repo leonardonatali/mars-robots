f1f6a3287d543e0fdb4b72b70b9f31251cb6f1de
Leonardo Natali <leonardo.natali@hotmail.com>
2021-08-17 20:30:37 -0300

    fix: remove not used controllers and adjusts dependencies

--- src/main/java/com/leonardonatali/marsrobots/terrain/dto/SendCommandToRobotRequestDto.java
+++
@@ -1,5 +0,0 @@
-package com.leonardonatali.marsrobots.terrain.dto;
-
-public class SendCommandToRobotRequestDto {
-    public String command;
-}
--- src/main/java/com/leonardonatali/marsrobots/terrain/dto/SendCommandToRobotResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.leonardonatali.marsrobots.terrain.dto;
-
-public class SendCommandToRobotResponseDto {
-    public SendCommandToRobotResponseDto() {
-        super();
-    }
-}
