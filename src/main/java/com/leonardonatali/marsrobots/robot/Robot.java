525119fe4bb63fa49de69fee5e7d9e2b86a9bac7
Leonardo Natali <leonardo.natali@hotmail.com>
2021-08-17 22:14:42 -0300

    feat: add robot coordinates

--- src/main/java/com/leonardonatali/marsrobots/robot/Robot.java
+++ src/main/java/com/leonardonatali/marsrobots/robot/Robot.java
@@ -6,9 +6,41 @@ import com.leonardonatali.marsrobots.position.enums.PositionEnum;
 
 public class Robot {
     private PositionEnum position;
+    private int x;
+    private int y;
 
     public Robot() {
         this.position = PositionEnum.NORTH;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
     }
 
     public void turn(MoveEnum move) {
